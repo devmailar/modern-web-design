6c4e5abbb1145ef6617f374bf155a4d71e2844f7
devmailar <veex1233@gmail.com>
2022-01-28 14:54:43 +0200

    change description

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let bod = document.querySelector("body")
 
 window.addEventListener('load', (event) => {
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < 50; i++) {
     setTimeout(function timer() {
      bod.classList.toggle("dark")
      bod.classList.toggle("transition")
